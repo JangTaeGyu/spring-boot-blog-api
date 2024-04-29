58f29e4f157d0512709fc3725232c7e03696674a
JangTaeGyu <ttggbbgg2@gmail.com>
2024-04-29 20:31:29 +0900

    init

---
+++ src/test/java/com/example/springbootblogapi/SpringBootBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
