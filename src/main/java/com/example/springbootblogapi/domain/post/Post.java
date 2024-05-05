9c97601b4b659c3cd2e05a13ac95a579ab04f723
JangTaeGyu <ttggbbgg2@gmail.com>
2024-05-05 17:59:59 +0900

    Post Entity 추가

---
+++ src/main/java/com/example/springbootblogapi/domain/post/Post.java
@@ -0,0 +1,44 @@
+package com.example.springbootblogapi.domain.post;
+
+import com.example.springbootblogapi.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@Getter @NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+    @Column(nullable = false)
+    private Long categoryId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition="TEXT")
+    private String body;
+
+    @ColumnDefault("'false'")
+    private boolean show;
+
+    protected LocalDateTime deletedAt;
+
+    public Post(Long categoryId, String title, String body, boolean show) {
+        this.categoryId = categoryId;
+        this.title = title;
+        this.body = body;
+        this.show = show;
+    }
+
+    public static Post fakeEntity(Long id) {
+        Post post = new Post();
+        post.id = id;
+        return  post;
+    }
+}
