15d81448d44630a994a3fec52d4b4a1c38933cf0
JangTaeGyu <ttggbbgg2@gmail.com>
2024-05-06 09:26:34 +0900

    포스트 생성 서비스 테스트 추가

--- src/test/java/com/example/springbootblogapi/domain/post/PostCreatorTest.java
+++ src/test/java/com/example/springbootblogapi/domain/post/PostCreatorTest.java
@@ -1,12 +1,25 @@
 package com.example.springbootblogapi.domain.post;
 
+import com.example.springbootblogapi.mock.TestPostContainer;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PostCreatorTest {
+    private final PostCreator postCreator;
+
+    public PostCreatorTest() {
+        TestPostContainer testContainer = new TestPostContainer();
+        this.postCreator = testContainer.postCreator;
+    }
 
     @Test
+    @DisplayName("createPost - 포스트 생성")
     void createPost() {
+        PostData postData = new PostData(3L, "Post Title 11", "Post Body 11");
+        Long createdPostId = postCreator.createPost(postData);
+
+        assertThat(createdPostId).isPositive();
     }
 }
