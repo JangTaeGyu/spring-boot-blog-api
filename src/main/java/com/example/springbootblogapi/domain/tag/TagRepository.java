39ce2fd8c2d869abf303a9ecc2084a016cb5bc71
JangTaeGyu <ttggbbgg2@gmail.com>
2024-05-08 20:46:58 +0900

    Tag Entity 추가

---
+++ src/main/java/com/example/springbootblogapi/domain/tag/TagRepository.java
@@ -0,0 +1,9 @@
+package com.example.springbootblogapi.domain.tag;
+
+import java.util.Optional;
+
+public interface TagRepository {
+    Long create(Tag tag);
+
+    Optional<Tag> findByName(String name);
+}
