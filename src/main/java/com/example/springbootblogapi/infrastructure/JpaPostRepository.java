117916dc68669ecc91beb0a2062f729b30db5078
JangTaeGyu <ttggbbgg2@gmail.com>
2024-05-10 20:45:23 +0900

    포스트 수정 버그 수정

--- src/main/java/com/example/springbootblogapi/infrastructure/JpaPostRepository.java
+++ src/main/java/com/example/springbootblogapi/infrastructure/JpaPostRepository.java
@@ -1,6 +1,7 @@
 package com.example.springbootblogapi.infrastructure;
 
 import com.example.springbootblogapi.domain.post.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
@@ -8,5 +9,6 @@ import java.util.Optional;
 public interface JpaPostRepository extends Repository<Post, Long> {
     Post save(Post post);
 
+    @Query(value = "select p from Post p where p.id = :id and p.deletedAt is null")
     Optional<Post> findById(Long id);
 }
