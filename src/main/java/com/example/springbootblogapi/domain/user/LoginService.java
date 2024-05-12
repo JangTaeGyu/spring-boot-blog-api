ed424cc862339d4a283d3f2ed7a0ba8460ddee61
JangTaeGyu <ttggbbgg2@gmail.com>
2024-05-12 09:40:36 +0900

    LoginService 에 마지막 로그인 시간 저장

--- src/main/java/com/example/springbootblogapi/domain/user/LoginService.java
+++ src/main/java/com/example/springbootblogapi/domain/user/LoginService.java
@@ -10,11 +10,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class LoginService {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenManager jwtTokenManager;
+    private final UserRepository userRepository;
 
     private AccessTokenDto makeAccessTokenDto(Authentication authenticate) {
         CustomUserDetails userDetails = (CustomUserDetails) authenticate.getPrincipal();
@@ -27,6 +30,9 @@ public class LoginService {
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
             Authentication authenticate = authenticationManager.authenticate(token);
+
+            // 로그인 시간 저장
+            userRepository.updateLatestAccessedAtByEmail(request.getEmail(), LocalDateTime.now());
 
             return makeAccessTokenDto(authenticate);
         } catch (AuthenticationException e) {
--- src/main/java/com/example/springbootblogapi/domain/user/UserRepository.java
+++ src/main/java/com/example/springbootblogapi/domain/user/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.springbootblogapi.domain.user;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserRepository {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
     void create(User user);
+    void updateLatestAccessedAtByEmail(String email, LocalDateTime latestAccessedAt);
 }
