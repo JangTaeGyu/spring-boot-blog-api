56a70cac5ebc1979a72cd027a250d08de6a23565
JangTaeGyu <ttggbbgg2@gmail.com>
2024-05-12 09:43:41 +0900

    UserRepository 마지막 로그인 시간 수정 기능 추가

--- src/main/java/com/example/springbootblogapi/infrastructure/UserRepositoryImpl.java
+++ src/main/java/com/example/springbootblogapi/infrastructure/UserRepositoryImpl.java
@@ -2,17 +2,22 @@ package com.example.springbootblogapi.infrastructure;
 
 import com.example.springbootblogapi.domain.user.User;
 import com.example.springbootblogapi.domain.user.UserRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+
+import static com.example.springbootblogapi.domain.user.QUser.user;
 
 @Repository
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
     private final JpaUserRepository userRepository;
+    private final JPAQueryFactory query;
 
     @Override
     public boolean existsByEmail(String email) {
@@ -28,5 +33,17 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public void create(User user) {
         userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateLatestAccessedAtByEmail(String email, LocalDateTime latestAccessedAt) {
+        query.update(user)
+                .set(user.latestAccessedAt, latestAccessedAt)
+                .where(
+                        user.email.eq(email),
+                        user.deletedAt.isNull()
+                )
+                .execute();
     }
 }
