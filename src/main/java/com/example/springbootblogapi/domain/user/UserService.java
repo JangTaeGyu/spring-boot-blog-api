2564aa004f9b459886aaaf24a6dd21238e83aa46
JangTaeGyu <ttggbbgg2@gmail.com>
2024-05-13 09:23:09 +0900

    UserRepository 검색 기능 추가

--- src/main/java/com/example/springbootblogapi/domain/user/UserService.java
+++ src/main/java/com/example/springbootblogapi/domain/user/UserService.java
@@ -1,15 +1,24 @@
 package com.example.springbootblogapi.domain.user;
 
 import com.example.springbootblogapi.domain.user.data.UserCreateData;
+import com.example.springbootblogapi.domain.user.data.UserSearchData;
 import com.example.springbootblogapi.domain.user.data.UserUpdateData;
+import com.example.springbootblogapi.domain.user.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserQuery userQuery;
     private final UserCreator userCreator;
     private final UserUpdater userUpdater;
+
+    public Page<UserDto> searchUsersBy(UserSearchData searchData, Pageable pageable) {
+        return userQuery.searchUsersBy(searchData, pageable);
+    }
 
     public Long createUser(UserCreateData data) {
         return userCreator.createUser(data);
--- src/main/java/com/example/springbootblogapi/infrastructure/UserRepositoryImpl.java
+++ src/main/java/com/example/springbootblogapi/infrastructure/UserRepositoryImpl.java
@@ -2,12 +2,23 @@ package com.example.springbootblogapi.infrastructure;
 
 import com.example.springbootblogapi.domain.user.User;
 import com.example.springbootblogapi.domain.user.UserRepository;
+import com.example.springbootblogapi.domain.user.UserRole;
+import com.example.springbootblogapi.domain.user.data.UserSearchData;
+import com.example.springbootblogapi.domain.user.dto.QUserDto;
+import com.example.springbootblogapi.domain.user.dto.UserDto;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static com.example.springbootblogapi.domain.user.QUser.user;
@@ -18,6 +29,37 @@ import static com.example.springbootblogapi.domain.user.QUser.user;
 public class UserRepositoryImpl implements UserRepository {
     private final JpaUserRepository userRepository;
     private final JPAQueryFactory query;
+
+    private BooleanBuilder toBooleanBuilder(UserSearchData searchData) {
+        return new BooleanBuilder(user.deletedAt.isNull())
+                .and(containsKeyword(searchData.getKeyword()))
+                .and(eqRole(searchData.getRole()));
+    }
+
+    @Override
+    public Page<UserDto> searchBy(UserSearchData searchData, Pageable pageable) {
+        BooleanBuilder booleanBuilder = toBooleanBuilder(searchData);
+
+        List<UserDto> content = query.select(new QUserDto(
+                        user.id,
+                        user.email,
+                        user.name,
+                        user.latestAccessedAt,
+                        user.createdAt,
+                        user.updatedAt
+                ))
+                .from(user)
+                .where(booleanBuilder)
+                .offset(pageable.getOffset()).limit(pageable.getPageSize())
+                .orderBy(user.createdAt.desc())
+                .fetch();
+
+        JPAQuery<Long> count = query.select(user.count())
+                .from(user)
+                .where(booleanBuilder);
+
+        return PageableExecutionUtils.getPage(content, pageable, count::fetchOne);
+    }
 
     @Override
     public boolean existsByEmail(String email) {
@@ -50,5 +92,15 @@ public class UserRepositoryImpl implements UserRepository {
                         user.deletedAt.isNull()
                 )
                 .execute();
+    }
+
+    public BooleanExpression containsKeyword(String keyword) {
+        if (keyword == null || keyword.isEmpty()) return null;
+        return user.email.contains(keyword).or(user.name.contains(keyword));
+    }
+
+    public BooleanExpression eqRole(UserRole role) {
+        if (role == null) return null;
+        return user.role.eq(role);
     }
 }
