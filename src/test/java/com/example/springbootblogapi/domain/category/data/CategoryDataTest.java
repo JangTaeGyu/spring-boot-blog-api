c0bead8a73f2e98d41a0b0b858a6ce8fdc01815f
JangTaeGyu <ttggbbgg2@gmail.com>
2024-05-23 20:03:14 +0900

    CategoryData 테스트 코드 추가

---
+++ src/test/java/com/example/springbootblogapi/domain/category/data/CategoryDataTest.java
@@ -0,0 +1,44 @@
+package com.example.springbootblogapi.domain.category.data;
+
+import com.example.springbootblogapi.domain.category.Category;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class CategoryDataTest {
+    @DisplayName("createCategoryData - 카테고리 데이터 생성 오류 - IllegalArgumentException")
+    @Test
+    void createCategoryData_IllegalArgumentException() {
+        assertThatThrownBy(() -> new CategoryData("", null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new CategoryData(null, "")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new CategoryData(null, null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("createCategoryData - 카테고리 데이터 생성")
+    @Test
+    void createCategoryData() {
+        String categoryName = "category name";
+        String categoryDescription = "category description";
+
+        CategoryData data = new CategoryData(categoryName, categoryDescription);
+
+        assertThat(data.getName()).isEqualTo(categoryName);
+        assertThat(data.getDescription()).isEqualTo(categoryDescription);
+    }
+
+    @DisplayName("toEntity - CategoryData 에서 Category Entity 생성")
+    @Test
+    void toEntity() {
+        String categoryName = "category name";
+        String categoryDescription = "category description";
+
+        CategoryData data = new CategoryData(categoryName, categoryDescription);
+        Category category = data.toEntity();
+
+        assertThat(category).isInstanceOf(Category.class);
+        assertThat(category.getName()).isEqualTo(categoryName);
+        assertThat(category.getDescription()).isEqualTo(categoryDescription);
+    }
+}
